fe028ff9fb7909595fd74edc4613451115a3655c
Stanley <stanley.yang.sy@gmail.com>
2012-11-17 05:40:29 -0500

    changed request type, improved webpage

--- code.js
+++ code.js
@@ -1,34 +1,39 @@
-
-function updateDisplay(message){
-	display.value = message + "\n \n" + display.value;
+var map;
+var marist = new google.maps.LatLng(41.722667,-73.934256);
+var textRequest = {
+		location: marist,
+		query: "food",
+        radius: 500,
+        };
+		
+function initialize() {
+    map = new google.maps.Map(document.getElementById("map"), {
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		center: marist,
+        zoom: 15
+        });
 }
 
-function onClickCommands(){
-var maristCor = new google.maps.LatLng( 41.722667,-73.934256);
+function printLocations(){
+    var service = new google.maps.places.PlacesService(map);
+    service.textSearch(textRequest, callback);
 }
 
-function printout(value){
-for(var i = 0; i<value.length; i++){
-console.log(value[i]);
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        }
+    }
+	console.log("");
 }
+
+function changeValue(value){
+if (value != ""){
+	textRequest.query = value;
+	input.value = "";
+	}
+printLocations();
 }
 
-function test(){
-
-var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.722667,-73.934256&radius=500&types=food&sensor=false&key=AIzaSyCjbp32tQBOwSaU0g65BYpM30ezLQ93dxQ';
-
-$.getJSON(test.json, function(data){
-  var items = [];
-console.log("RAN");
-  $.each(data, function(key, val) {
-    items.push('<li id="' + key + '">' + val + '</li>');
-  });
-
-  $('<ul/>', {
-    'class': 'my-new-list',
-    html: items.join('')
-  }).appendTo('body');
-});
-
-
-}
+google.maps.event.addDomListener(window, "load", initialize);
