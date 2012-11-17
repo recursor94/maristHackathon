e8804b7b19a474c451896a55c28df04190f84b3a
Stanley <stanley.yang.sy@gmail.com>
2012-11-17 06:48:37 -0500

    added public output, added addresses, fixed addresses

--- code.js
+++ code.js
@@ -3,8 +3,9 @@ var marist = new google.maps.LatLng(41.722667,-73.934256);
 var textRequest = {
 		location: marist,
 		query: "food",
-        radius: 500,
+        radius: "500",
         };
+		
 		
 function initialize() {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -16,24 +17,49 @@ function initialize() {
 
 function printLocations(){
     var service = new google.maps.places.PlacesService(map);
+    service.textSearch(textRequest, callbackTen);
+}
+
+
+function printAllLocations(){
+    var service = new google.maps.places.PlacesService(map);
     service.textSearch(textRequest, callback);
 }
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
 		for (var i = 0; i < results.length; i++) {
-        console.log(results[i].name);
+        output.value += results[i].name + "\n";
+		output.value += removingExtraUS(results[i].formatted_address) + "\n";
         }
     }
-	console.log("");
 }
 
-function changeValue(value){
-if (value != ""){
-	textRequest.query = value;
-	input.value = "";
+function callbackTen(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < 10; i++) {
+        output.value += results[i].name + "\n";
+		output.value += removingExtraUS(results[i].formatted_address) + "\n";
+        }
+    }
+}
+
+function removingExtraUS(original){
+	var temp = original;
+	for(var i=1; i < original.length; i++){
+		if(temp.substring(0, 4) === " NY,") break;
+		temp = temp.substring(1);
 	}
-printLocations();
+	return original.substring(0, i - 2);
+}	
+
+function changeValue(value){
+	if (value != ""){
+		textRequest.query = value;
+		input.value = "";
+	}
+	output.value = "";
+	printLocations();
 }
 
 google.maps.event.addDomListener(window, "load", initialize);
