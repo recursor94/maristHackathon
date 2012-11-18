f1d83f96c15e8d6ad136ebfb9210bd8b0f6b6396
Antony Liang <find.aliang@gmail.com>
2012-11-17 19:16:37 -0500

    added moreInfo()

--- code.js
+++ code.js
@@ -1,13 +1,26 @@
 var map;
 var marist = new google.maps.LatLng(41.722667,-73.934256); //roughly the center of marist
+
 var textRequest = {
     location: marist,
     query: "food",
     radius: "500",
 }; //default request object, searches for query in a 500m radius around marist
-var addresses = [];
 var formattedAddress = [];
 var dynamicID = "output";
+var dynamicBtn = "outputbtn";
+var dynamicInfo = "info";
+var info = {
+    rating: null,
+    phone: "not given",
+    website: "not given",
+};
+var placeArray = [];
+function place (rating, phone, website){
+    this.rating = rating;
+    this.phone = phone;
+    this.website = website;
+}
 
 //initializes map
 function initialize() {
@@ -29,20 +42,58 @@ function callbackTen(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
 	for (var i = 0; i < 10; i++) {
 	    dynamicID += i;
-	    addresses.push(results[i].formatted_address);
+	    dynamicBtn += i; 
 
-//	    output.value += results[i].name + "\n";
 	    document.getElementById(dynamicID).value += results[i].name + "\n";
 
 	    removedUS = removingExtraUS(results[i].formatted_address) + "\n";
 	    formattedAddress.push(removedUS);
 	    document.getElementById(dynamicID).value += removedUS;
+	    
+	    var r = 0;
+	    var p = "Not given";
+	    var w = "Not given";
+	  
+	    if(results[i].rating != undefined) r = results[i].rating;
+	    if(results[i].phone != undefined) p = results[i].phone;
+	    if(results[i].website != undefined) w = results[i].website;
 
-//	    console.log(addresses[i]);
-//	    console.log(formattedAddress[i]);
+	    var temp = new place(r, p, w);
+	    placeArray.push(temp);
+
+	    console.log("rating: " + results[i].rating);
+/*
+	    console.log(results[i]);
+            console.log(results[i].types);//gives an array of it's tags
+	    if(results[i].opening_hours != undefined){
+		console.log("opening now: " + results[i].opening_hours.open_now);//returns if open now or not
+	    }
+	    console.log("rating: " + results[i].rating);
+	    console.log("formatted address: " + results[i].formatted_address);
+	    console.log("website: " + results[i].website);
+*/
+
 	    dynamicID = dynamicID.substring(0,6);
+	    dynamicBtn = dynamicBtn.substring(0,9);
 	}
     }
+}
+
+function moreInfo(index,id){
+//    console.log(placeArray[index].rating);
+/*
+    tagid = dynamicInfo + index;
+    console.log(document.getElementById(tagid));
+*/
+    var element = document.getElementById(id);
+
+    if(element.style.display == 'none'){
+	element.style.display = 'block';
+    }
+    else{
+	element.style.display = 'none'; 
+    }
+//    dynamicInfo = dynamicInfo.substring(0,4);
 }
 
 //cuts off the NY, United States in the address, returns the address with out it
@@ -61,11 +112,16 @@ function changeValue(value){
 	textRequest.query = value;
 	input.value = "";
     }
-  //  output.value = "";
+    for(var i = 0; i < 10; i++){
+	dynamicID += i;
+	document.getElementById(dynamicID).value = "";
+	dynamicID = dynamicID.substring(0,6);
+    }
     for(var i = 0; i < 10; i++){
 	dynamicID += i;
 	dynamicID = dynamicID.substring(0,6);
     }
+    
     printLocations();
 }
 
@@ -89,17 +145,16 @@ function doClick(buttonName,e){
     }
 }
 
-function getDirections(){
-
-
-}
-
 window.onload = function() {
 //    document.getElementById('button').onclick = changeValue(input.value);
     document.getElementById('input').onkeypress = function(e) {
-	doClick('button', e);
+	doClick('button', e)
     };
-
+    for(var i = 0; i < 9; i++){
+	dynamicInfo += i;
+	document.getElementById("info" + i).style.display = 'none';
+	dynamicInfo = dynamicInfo.substring(0,4);
+    }
 };
 
 google.maps.event.addDomListener(window, "load", initialize);
