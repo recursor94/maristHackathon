f0aec88e424755e3b966faa82873baf34da3d6a7
Andrew Spano <andrew.spano1@marist.edu>
2013-05-03 22:33:39 -0400

    created graph.js file to store the format for the graph

---
+++ gui/graph.js
@@ -0,0 +1,24 @@
+/* This is the code for the graph that plots the ten game server
+ ** integer outputs, which are server info plus profit. */
+
+//written by Andrew Spano, uses flot.js and jquery.js
+
+$.plot (
+    $("#statusgraph"),
+    [
+        {
+            label: "Demand per region",
+            data: [ [0, 0], [1, 1], [2, 1], [3,2] ],
+            lines: {show: true},
+            points: {show: true}
+
+
+        },
+        {
+            label: "Number of Webservers",
+            data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+            lines: {show: true},
+            points: {show: true}   
+        }
+    ]
+);
