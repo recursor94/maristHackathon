d132c809c3e0226f2dc835dcc8f4c1dbe379d2f7
Andrew Spano <andrew.spano1@marist.edu>
2013-05-03 21:13:27 -0400

    added data txt file for editing purpose

--- gui/filopen.js
+++ gui/filopen.js
@@ -1,8 +1,10 @@
-var txt = ''
-var xmlhttp.onreadystatechange = function () {
+var txt = '';
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
     if(xmlhttp.status==200 && xmlhttp.readyState==4) {
         txt=xmlhttp.responseText;
     }
 };
 xmlhttp.open("GET", "data.txt", true);
 xmlhttp.send();
+document.write(txt);
