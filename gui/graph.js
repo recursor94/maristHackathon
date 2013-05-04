fced26759fc63b40ca5cf089192212eeda950f0a
Andrew Spano <andrew.spano1@marist.edu>
2013-05-03 23:40:27 -0400

    finished profit skeleton

--- gui/graph.js
+++ gui/graph.js
@@ -10,7 +10,7 @@ $.plot (
     $("#placeholder"),
     [
         {
-            label: "Demand per region",
+            label: "Demand in the United States",
             data: [ [0, 0], [1, 1], [2, 1], [3,2] ],
             lines: {show: true},
             points: {show: true}
@@ -18,11 +18,96 @@ $.plot (
 
         },
         {
-            label: "Number of Webservers",
-            data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+            label: "Demand in the European Union",
+            data: [[0, 0], [1, 3], [2,5] [3, 8]],
+            lines: {show: true},
+            points: {show: true}
+         },
+
+        {
+
+            label: "Demand in the Asian Pacific",
+            data: [[0, 0], [1, 10], [2,4], [3,9]],
+            lines: {show: true},
+            points: {show: true}
+
+        },
+
+
+         {
+             label: "Number of Web Servers in North America",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+         },
+
+         {
+             label: "Number of Web Servers in the European Union",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+
+         {
+             label: "Number of Web Servers in the Asian Pacific",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+
+         {
+             label: "Number of Java Servers in the United States",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+         {
+             label: "Number of Java Servers in the European Union",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+         {
+             label: "Number of Java Servers in the Asian Pacific",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+         {
+             label: "Number of Database Servers in the United States",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+         {
+             label: "Number of Database Servers in the European Union",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+         {
+             label: "Number of Database Servers in the Asian Pacific",
+             data: [ [0, 0], [1, 4], [2, 15], [3,0] ],
+             lines: {show: true},
+             points: {show: true}   
+
+         },
+
+         {
+            label: "Profit",
+            data: [ [0, 0], [1, 5.3], [2, 13.53], [3, 8.78] ],
             lines: {show: true},
             points: {show: true}   
+
         }
+          
     ]
 );
 }
