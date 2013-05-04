719ecd1b4e3ceeb5b1294baf6dfcd87f86cdcfca
Andrew Spano <andrew.spano1@marist.edu>
2013-05-04 02:25:24 -0400

    added feed data and jazz

---
+++ gui/feeddata.js
@@ -0,0 +1,22 @@
+var dat  = new Array();
+
+
+$(document).ready(function () {
+    
+    dat = [ [0, 0], [1,14], [2, 8], [3, 6]];
+    draw_graph();
+    update_data(1, dat);
+    update_data(2, dat);
+    update_data(3, dat);
+    update_data(4, dat);
+    update_data(5, dat);
+    update_data(6, dat);
+    update_data(7, dat);
+    update_data(8, dat);
+    update_data(9, dat);
+    update_data(10, dat);
+    update_data(11, dat);
+    update_data(12, dat);
+    draw_graph();
+}
+                 );
